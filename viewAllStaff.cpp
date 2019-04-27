27ab71e52b2894e8b354955f02f4aa13af9f5dde
jydizon <47217807+jydizon@users.noreply.github.com>
2019-04-27 17:37:57 +0800

    Create viewAllStaff.cpp

---
+++ viewAllStaff.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include<cstdio>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+    void viewAllStaff(void) {
+      ifstream fin;
+      fin.open("staffrec.txt");
+      if (fin.fail())
+      {
+          cout << "Error in file opening!" << endl;
+          exit(1);
+      }
+      cout<<"\n\t                                                           =======================";
+      cout<<"\n\t                                                            List of Staff Members";
+      cout<<"\n\t                                                           =======================\n";
+
+
+      string staffrecord[500][10];
+      for (int i=0; i<500; i++){
+        for (int j=0; j<10; j++){
+          fin >> staffrecord[i][j];
+        }
+      }
+
+      //outputting staff
+      cout << "===================================================================================================================================================================================";
+      for (int i=0; i<500; i++){
+        if (staffrecord[i][0].length() > 0 )
+        {
+        cout << "\n";
+        cout << "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
+        cout << endl << endl;
+        for (int j=0; j<10; j++){
+          cout << setw(8) <<staffrecord[i][j] << "     |" << "    " ;
+          }
+        cout << "\n";
+        }
+        else {
+          break;
+        }
+      }
+
+      cout << endl;
+      fin.close();
+      backToMenu();
+    }
